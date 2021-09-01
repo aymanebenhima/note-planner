58fe1ee0044b7e3cd6756b20bbd73a79a9f6fe4d
Aymane Benhima <aymanebenhima@gmail.com>
2021-09-01 15:22:42 +0100

    filtering project to all completed and ongoing

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="filter-nav">
+      <button :class="{ active: current === 'all' }" @click="updateFilter('all')">View All</button>
+      <button :class="{ active: current === 'completed' }" @click="updateFilter('completed')">Completed</button>
+      <button :class="{ active: current === 'ongoing' }" @click="updateFilter('ongoing')">Ongoing</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['current'],
+    methods: {
+        updateFilter(by) {
+            this.$emit('filterChange', by)
+        }
+    },
+}
+</script>
+
+<style>
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #7f8c8d;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+    color: #34495e;
+}
+</style>
